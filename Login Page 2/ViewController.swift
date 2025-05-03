2034ab7ca57eb3691283890eb6f80bb11d0c30f4
festimbunjaku <festimbu@gmail.com>
2025-05-04 00:28:24 +0200

    Initial Commit

---
+++ Login Page 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login Page 2
+//
+//  Created by Festim Bunjaku on 5/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Login Page 2Tests/Login_Page_2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Page_2Tests.swift
+//  Login Page 2Tests
+//
+//  Created by Festim Bunjaku on 5/4/25.
+//
+
+import Testing
+@testable import Login_Page_2
+
+struct Login_Page_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
